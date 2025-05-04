29d2f0ff909a365d3b18b5321dacf76d1a6666a2
Tushar Holkar <134801002+tusharholkar08@users.noreply.github.com>
2025-05-05 00:18:42 +0530

    Create DFS.cpp

---
+++ HPC/Error Solved/DFS.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <omp.h>
+
+using namespace std;
+
+const int MAX = 100000;
+vector<int> graph[MAX];
+bool visited[MAX];
+
+void dfs(int node) {
+    stack<int> s;
+    s.push(node);
+
+    while (!s.empty()) {
+        int curr_node = s.top();
+        s.pop();
+
+        if (!visited[curr_node]) {
+            visited[curr_node] = true;
+            cout << curr_node << " ";
+
+            // Push neighbors (no need to parallelize this part in DFS)
+            for (int i = graph[curr_node].size() - 1; i >= 0; i--) {
+                int adj_node = graph[curr_node][i];
+                if (!visited[adj_node]) {
+                    s.push(adj_node);
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int n, m, start_node;
+    cout << "Enter No of Nodes, Edges, and start node: ";
+    cin >> n >> m >> start_node;
+
+    cout << "Enter Pair of edges: ";
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    // Parallel initialization of visited array
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        visited[i] = false;
+    }
+
+    // Print number of threads used
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            cout << "\nNumber of threads used: " << omp_get_num_threads() << endl;
+        }
+    }
+
+    cout << "\nDFS traversal: ";
+    dfs(start_node);
+
+    return 0;
+}
